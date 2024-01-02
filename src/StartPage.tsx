4dad298148137f9a377549740b898d97fad64021
pifflox <daiyanrafi1@gmail.com>
2024-01-02 21:25:17 +0600

    home-3 starpage button and design fixed

--- src/StartPage.tsx
+++ src/StartPage.tsx
@@ -37,54 +37,57 @@ const StartPage: React.FC<StartPageProps> = ({ onNext }) => {
   };
 
   return (
-    <Container>
-      <Paper elevation={3} style={{ padding: '2rem', marginTop: '2rem' }}>
+    <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Paper elevation={3} style={{ padding: '2rem', marginTop: '2rem'}}>
         <Typography variant="h6" gutterBottom>
           Questionnaire:
         </Typography>
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Are you without electricity, gas, or water?</FormLabel>
-          <RadioGroup
-            row
-            aria-label="question1"
-            name="question1"
-            value={selectedOption1}
-            onChange={(e) => handleChange1(e.target.value)}
-          >
-            <FormControlLabel value="yes1" control={<Radio />} label="Yes" />
-            <FormControlLabel value="no1" control={<Radio />} label="No" />
-          </RadioGroup>
-          {showWarning && (
-            <Typography color="error" variant="body2">
-              Please contact your foreman at {selectedOption1 === 'yes1' ? '+376423926719' : '+00393946334'}
-            </Typography>
-          )}
-        </FormControl>
 
-        {selectedOption1 === 'no1' && (
+        <div style={{ marginBottom: '2rem' }}>
           <FormControl component="fieldset">
-            <FormLabel component="legend">Have you contacted your provider to try to resolve your complaint?</FormLabel>
+            <FormLabel component="legend">Are you without electricity, gas, or water?</FormLabel>
             <RadioGroup
-              row
-              aria-label="question2"
-              name="question2"
-              value={selectedOption2}
-              onChange={(e) => handleChange2(e.target.value)}
+              aria-label="question1"
+              name="question1"
+              value={selectedOption1}
+              onChange={(e) => handleChange1(e.target.value)}
             >
-              <FormControlLabel value="yes2" control={<Radio />} label="Yes" />
-              <FormControlLabel value="no2" control={<Radio />} label="No" />
+              <FormControlLabel value="yes1" control={<Radio />} label="Yes" />
+              <FormControlLabel value="no1" control={<Radio />} label="No" />
             </RadioGroup>
             {showWarning && (
               <Typography color="error" variant="body2">
-                Please contact your foreman at +00393946334
+                Please contact your foreman at {selectedOption1 === 'yes1' ? '+376423926719' : '+00393946334'}
               </Typography>
             )}
-            {selectedOption2 === 'yes2' && (
-              <Button variant="contained" color="primary" onClick={handleNext} style={{ marginTop: '2rem' }}>
-                Next
-              </Button>
-            )}
           </FormControl>
+        </div>
+
+        {selectedOption1 === 'no1' && (
+          <div>
+            <FormControl component="fieldset">
+              <FormLabel component="legend">Have you contacted your provider to try to resolve your complaint?</FormLabel>
+              <RadioGroup
+                aria-label="question2"
+                name="question2"
+                value={selectedOption2}
+                onChange={(e) => handleChange2(e.target.value)}
+              >
+                <FormControlLabel value="yes2" control={<Radio />} label="Yes" />
+                <FormControlLabel value="no2" control={<Radio />} label="No" />
+              </RadioGroup>
+              {showWarning && (
+                <Typography color="error" variant="body2">
+                  Please contact your foreman at +00393946334
+                </Typography>
+              )}
+              {selectedOption2 === 'yes2' && (
+                <Button variant="contained" color="primary" onClick={handleNext} style={{ marginTop: '2rem' }}>
+                  Next
+                </Button>
+              )}
+            </FormControl>
+          </div>
         )}
       </Paper>
     </Container>
--- src/X-FormWrapper.tsx
+++ src/X-FormWrapper.tsx
@@ -18,3 +18,59 @@
 //     </Container>
 //   );
 // }
+
+
+//start page wrong allignment code
+// return (
+//     <Container>
+//       <Paper elevation={3} style={{ padding: '2rem', marginTop: '2rem' }}>
+//         <Typography variant="h6" gutterBottom>
+//           Questionnaire:
+//         </Typography>
+//         <FormControl component="fieldset">
+//           <FormLabel component="legend">Are you without electricity, gas, or water?</FormLabel>
+//           <RadioGroup
+//             row
+//             aria-label="question1"
+//             name="question1"
+//             value={selectedOption1}
+//             onChange={(e) => handleChange1(e.target.value)}
+//           >
+//             <FormControlLabel value="yes1" control={<Radio />} label="Yes" />
+//             <FormControlLabel value="no1" control={<Radio />} label="No" />
+//           </RadioGroup>
+//           {showWarning && (
+//             <Typography color="error" variant="body2">
+//               Please contact your foreman at {selectedOption1 === 'yes1' ? '+376423926719' : '+00393946334'}
+//             </Typography>
+//           )}
+//         </FormControl>
+
+//         {selectedOption1 === 'no1' && (
+//           <FormControl component="fieldset">
+//             <FormLabel component="legend">Have you contacted your provider to try to resolve your complaint?</FormLabel>
+//             <RadioGroup
+//               row
+//               aria-label="question2"
+//               name="question2"
+//               value={selectedOption2}
+//               onChange={(e) => handleChange2(e.target.value)}
+//             >
+//               <FormControlLabel value="yes2" control={<Radio />} label="Yes" />
+//               <FormControlLabel value="no2" control={<Radio />} label="No" />
+//             </RadioGroup>
+//             {showWarning && (
+//               <Typography color="error" variant="body2">
+//                 Please contact your foreman at +00393946334
+//               </Typography>
+//             )}
+//             {selectedOption2 === 'yes2' && (
+//               <Button variant="contained" color="primary" onClick={handleNext} style={{ marginTop: '2rem' }}>
+//                 Next
+//               </Button>
+//             )}
+//           </FormControl>
+//         )}
+//       </Paper>
+//     </Container>
+//   );
